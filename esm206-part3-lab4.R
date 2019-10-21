d502f19641f3e1a31ab17c716b08c49297a11448
jaleisehall <jaleise_hall@ucsb.edu>
2019-10-21 13:57:00 -0700

    add infant mortality graph

---
+++ esm206-part3-lab4.R
@@ -0,0 +1,30 @@
+# ---------- # ---------- #
+# Jaleise Hall #
+# ---------- # ---------- #
+
+library(tidyverse)
+library(janitor)
+library(here)
+
+# Read in data:
+db <- read_csv(here::here("data", "disease_burden.csv")) %>% 
+  clean_names() %>% 
+  rename(deaths_per_100k = death_rate_per_100_000)
+
+# Create a subset, then graph:
+
+db_sub <- db %>% 
+  filter(country_name %in% c("United States", "Japan", "Afghanistan", "Somalia")) %>% 
+  filter(age_group == "0-6 days", sex == "Both")
+
+# Create a basic line graph:
+ggplot(data = db_sub, aes(x = year, y = deaths_per_100k)) +
+  geom_line(aes(color = country_name))
+
+
+
+
+
+
+
+
